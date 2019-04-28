9a83cc7dcddab37f510cce2d9d20c9ffa96d3b49
wuodland <karan_baruah@yahoo.co.in>
2019-04-28 17:00:15 +0700

    updated new script

--- RAM.sh
+++ RAM.sh
@@ -1,7 +1,10 @@
 for i in `seq 0 60`; do
-  #echo "Test done on `date`" >> ramusage.txt
-  #echo `cat /proc/meminfo | grep Active: | sed 's/Active: //g'`  >> ramusage.txt
-  #echo "" >> ramusage.txt
-  echo "CPU `LC_ALL=C top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}'`% RAM `free -m | awk '/Mem:/ { printf("%3.1f%%", $3/$2*100) }'` HDD `df -h / | awk '/\// {print $(NF-1)}'`" >> usage.txt
-  sleep 1m
+CPU_USAGE=$"`LC_ALL=C top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}'`% RAM `free -m | awk '/Mem:/ { printf("%3.1f%%", $3/$2*100) }'` HDD `df -h / | awk '/\// {print $(NF-1)}'`"
+
+DATE=$(date "+%Y-%m-%d %H:%M:")
+
+CPU_USAGE="$DATE CPU: $CPU_USAGE"
+
+echo $CPU_USAGE >> cpu.txt
+sleep 1m
 done
